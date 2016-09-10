21535b1260f633a5afba1a93d40ca51341725eba
Michael Chen <michael@admins-MacBook-Pro.local>
2016-09-09 22:35:26 -0700

    CSST related issues.

--- app/services/BuildRedshiftQuery.scala
+++ app/services/BuildRedshiftQuery.scala
@@ -9,8 +9,7 @@ trait BuildRedshiftQuery { this: PlainSqlRedshift =>
 
     def testFunc(implicit session: Session): String = {
         println("Testing: KV value returned by DB...")
-        val res = queryKeyLongValuePairs(session, "seven_day_reach_delta")
-        res.toString()
+        "OK"
     }
 
     def querySingleLong(implicit session: Session, view_name: String, postClause: String = "") : String = {
@@ -30,84 +29,33 @@ trait BuildRedshiftQuery { this: PlainSqlRedshift =>
 
     def queryTotalVideoViews(implicit session: Session, projID: Long) : String = {
         val numberFromQuery = StaticQuery[StatLong] +
-            "SELECT SUM(value) AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND proj_id_plat like '" + projID.toString +
-            "' AND title LIKE 'Lifetime Total Video Views' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%');"
+            "SELECT SUM(value) AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND proj_id_plat LIKE '" + projID.toString +
+            "' AND title LIKE 'Lifetime Total Video Views' AND sys_time > (CURRENT_DATE-2)  GROUP BY sys_time ORDER BY sys_time DESC LIMIT 1;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryTotalUniqueVideoViews(implicit session: Session, projID: Long) : String = {
-        val numberFromQuery = StaticQuery[StatLong] + "SELECT SUM(value) AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND proj_id_plat like '" + projID.toString +
-            "' AND title LIKE 'Lifetime Unique Video Views' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%');"
+        val numberFromQuery = StaticQuery[StatLong] +
+            "SELECT SUM(value) AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND proj_id_plat like '" + projID.toString +
+            "' AND title LIKE 'Lifetime Unique Video Views' AND sys_time > (CURRENT_DATE-2)  GROUP BY sys_time ORDER BY sys_time DESC LIMIT 1;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryInteractions(implicit session: Session, projID: Long) : String = {
-        val reactions = StaticQuery + "DROP VIEW IF EXISTS total_reactions; " +
-            "CREATE VIEW total_reactions AS SELECT 'total reactions' AS name, SUM( CONVERT(int, json_extract_path_text(value, 'love')) " +
-            "CONVERT(int, json_extract_path_text(value, 'haha')) " +
-            "CONVERT(int, json_extract_path_text(value, 'like')) " +
-            "CONVERT(int, json_extract_path_text(value, 'sorry')) " +
-            "CONVERT(int, json_extract_path_text(value, 'anger')) " +
-            "CONVERT(int, json_extract_path_text(value, 'wow')) ) AS sum FROM fb_insights "
-        "WHERE stats_type = 'VideoInsights' AND (title LIKE 'Lifetime Reactions by type') AND proj_id_plat LIKE '" + projID.toString +
-            "' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%');"
-
-        val shareComments = StaticQuery + "DROP VIEW IF EXISTS total_share_comments;" +
-            "CREATE VIEW total_share_comments AS SELECT 'total share and comments' AS name, " +
-            "SUM( nullif(json_extract_path_text(value, 'share'), ' ')::int + nullif(json_extract_path_text(value, 'comment'), ' ')::int ) AS sum " +
-            "FROM fb_insights WHERE stats_type = 'VideoInsights' AND (title LIKE 'Lifetime Video Stories by action type') AND proj_id_plat LIKE '" + projID.toString +
-            "' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%');"
-
-        val numberFromQuery = StaticQuery[StatLong] + "SELECT ((SELECT sum FROM total_reactions) + (SELECT sum FROM total_share_comments)) AS sum;"
+        val numberFromQuery = StaticQuery[StatLong] + "SELECT ((SELECT sum FROM total_reactions_" + projID.toString + ") + (SELECT sum FROM total_share_comments_" + projID.toString + ")) AS sum;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryTotalFollowers(implicit session: Session, projID: Long) : String = {
         val numberFromQuery = StaticQuery[StatLong] + "SELECT value AS sum FROM fb_insights WHERE title LIKE 'page_fan_count' AND proj_id_plat LIKE '" + projID.toString +
-            "' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%');"
+            "' AND sys_time > (CURRENT_DATE-2) ORDER BY sys_time DESC LIMIT 1;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryTotalReach(implicit session: Session, projID: Long) : String = {
         val numberFromQuery = StaticQuery[StatLong] + "SELECT SUM(value) AS sum FROM fb_insights " +
             "WHERE stats_type = 'VideoInsights' AND title LIKE 'Lifetime Video Total Reach' " +
-            "AND proj_id_plat LIKE '" + projID.toString + "' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%');"
+            "AND proj_id_plat LIKE '" + projID.toString + "' AND sys_time > (CURRENT_DATE-2) GROUP BY sys_time ORDER BY sys_time DESC LIMIT 1;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryTrendsData(implicit session: Session, projID: Long) : List[KeyLongValuePair] = {
-
-        val reach = StaticQuery + "DROP VIEW IF EXISTS seven_day_reach_delta;" + "" +
-            "CREATE VIEW seven_day_reach_delta AS " +
-            "SELECT 'seven-day-reach today' AS name, SUM(value) AS sum FROM fb_insights " +
-            "WHERE stats_type = 'Insights' AND title = 'Daily Total Reach' AND proj_id_plat LIKE '" + projID.toString +
-            "' AND end_time > (CURRENT_DATE-7) AND end_time < (CURRENT_DATE) UNION ALL " +
-            "SELECT 'seven-day-reach yesterday' AS name, SUM(value) AS sum FROM fb_insights WHERE stats_type = 'Insights' " +
-            "AND title = 'Daily Total Reach' AND proj_id_plat LIKE '" + projID.toString +
-            "' AND end_time > (CURRENT_DATE-8) AND end_time < (CURRENT_DATE-1);"
-
-        val views = StaticQuery + "DROP VIEW IF EXISTS seven_day_views_delta; CREATE VIEW seven_day_views_delta AS SELECT 'seven-day-views today' AS name, SUM(value) AS sum FROM fb_insights WHERE stats_type = 'Insights' AND title LIKE 'Daily Total Video Views' AND proj_id_plat LIKE '" + projID.toString +
-        "' AND end_time > (CURRENT_DATE-7) AND end_time < (CURRENT_DATE) UNION ALL SELECT 'seven-day-views yesterday' AS name, SUM(value) AS sum FROM fb_insights WHERE stats_type = 'Insights' AND title LIKE 'Daily Total Video Views' AND proj_id_plat LIKE '" + projID.toString +
-        "' AND end_time > (CURRENT_DATE-8) AND end_time < (CURRENT_DATE-1);"
-
-        val avgTime = StaticQuery + "DROP VIEW IF EXISTS seven_day_view_time_delta; CREATE VIEW seven_day_view_time_delta AS SELECT 'seven-day-view-time today' AS name, CONVERT(int,SUM(value)/7000) AS sum FROM fb_insights WHERE stats_type = 'DailyDiff' AND (title LIKE '%Total Video View Time%') AND proj_id_plat LIKE '" + projID.toString +
-        "' AND sys_time > (CURRENT_DATE-7) AND sys_time < (CURRENT_DATE) UNION ALL SELECT 'seven-day-view-time yesterday' AS name, CONVERT(int,SUM(value)/7000) AS sum FROM fb_insights WHERE stats_type = 'DailyDiff' AND (title LIKE '%Total Video View Time%') AND proj_id_plat LIKE '" + projID.toString +
-            "' AND sys_time > (CURRENT_DATE-8) AND sys_time < (CURRENT_DATE-1);"
-
-        val actionToday = StaticQuery + "DROP VIEW IF EXISTS seven_day_unique_share_today; create view seven_day_unique_share_today as select 'seven_day_unique_share today' as name, sum(json_extract_path_text(value, 'share')) as sum from fb_insights where stats_type = 'Insights' AND proj_id_plat LIKE '" + projID.toString +
-        "' and end_time > (CURRENT_DATE-7) AND end_time < (CURRENT_DATE) and name like '%page_admin_num_posts_by_type%' and json_extract_path_text(value, 'share') <> '' union all select 'seven_day_unique_like today' as name, sum(json_extract_path_text(value, 'like')) as sum from fb_insights where stats_type = 'Insights' AND proj_id_plat LIKE '" + projID.toString +
-        "' and end_time > (CURRENT_DATE-7) AND end_time < (CURRENT_DATE) and name like '%page_positive_feedback_by_type_unique%' and json_extract_path_text(value, 'like') <> '' union all select 'seven_day_unique_comment today' as name, sum(json_extract_path_text(value, 'comment')) as sum from fb_insights where stats_type = 'Insights' AND proj_id_plat LIKE '" + projID.toString +
-        "' and end_time > (CURRENT_DATE-7) AND end_time < (CURRENT_DATE) and name like '%page_positive_feedback_by_type_unique%' and json_extract_path_text(value, 'comment') <> '';"
-
-        val actionYesterday = StaticQuery + "DROP VIEW IF EXISTS seven_day_unique_share_yesterday; create view seven_day_unique_share_yesterday as select 'seven_day_unique_share yesterday' as name, sum(json_extract_path_text(value, 'share')) as sum from fb_insights where stats_type = 'Insights' AND proj_id_plat LIKE '" + projID.toString +
-        "' and end_time > (CURRENT_DATE-8) AND end_time < (CURRENT_DATE-1) and name like '%page_admin_num_posts_by_type%' and json_extract_path_text(value, 'share') <> '' union all select 'seven_day_unique_like yesterday' as name, sum(json_extract_path_text(value, 'like')) as sum from fb_insights where stats_type = 'Insights' AND proj_id_plat LIKE '" + projID.toString +
-        "' and end_time > (CURRENT_DATE-8) AND end_time < (CURRENT_DATE-1) and name like '%page_positive_feedback_by_type_unique%' and json_extract_path_text(value, 'like') <> '' union all select 'seven_day_unique_comment yesterday' as name, sum(json_extract_path_text(value, 'comment')) as sum from fb_insights where stats_type = 'Insights' AND proj_id_plat LIKE '" + projID.toString +
-        "' and end_time > (CURRENT_DATE-8) AND end_time < (CURRENT_DATE-1) and name like '%page_positive_feedback_by_type_unique%' and json_extract_path_text(value, 'comment') <> '';"
-
-        val actionDelta = StaticQuery + "DROP VIEW IF EXISTS seven_day_unique_actions_delta; create view seven_day_unique_actions_delta as select * from seven_day_unique_share_today union all select * from seven_day_unique_share_yesterday;"
-
-        val sevenDayTrends = StaticQuery + "DROP VIEW IF EXISTS seven_day_trends_data; " +
-            "CREATE VIEW seven_day_trends_data AS select * from seven_day_reach_delta " +
-            "union all select * from seven_day_views_delta " +
-            "union all select * from seven_day_view_time_delta " +
-            "union all select * from seven_day_unique_actions_delta order by name;"
-
-        val queryResult = StaticQuery[KeyLongValuePair] + "SELECT * FROM  seven_day_trends_data;"
+        val queryResult = StaticQuery[KeyLongValuePair] + "SELECT * FROM  seven_day_trends_data_" +  projID.toString + ";"
         queryResult.list
     }
     def queryTotalPosts(implicit session: Session, projID: Long) : String = {
@@ -117,12 +65,12 @@ trait BuildRedshiftQuery { this: PlainSqlRedshift =>
     }
     def queryAvgTimeViewed(implicit session: Session, projID: Long) : String = {
         val numberFromQuery = StaticQuery[StatLong] +
-            "SELECT AVG(value)/1000 AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND title LIKE 'Lifetime Average time video viewed' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%') AND proj_id_plat LIKE '" + projID.toString + "';"
+            "SELECT AVG(value)/1000 AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND title LIKE 'Lifetime Average time video viewed' AND sys_time > (CURRENT_DATE-2) AND proj_id_plat LIKE '" + projID.toString + "' GROUP BY sys_time ORDER BY sys_time DESC LIMIT 1;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryTotalTimeViewed(implicit session: Session, projID: Long) : String = {
         val numberFromQuery = StaticQuery[StatLong] +
-            "SELECT SUM(value)/1000 AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND title = 'Lifetime Total Video View Time (in MS)' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%') AND proj_id_plat LIKE '" + projID.toString + "';"
+            "SELECT SUM(value)/1000 AS sum FROM fb_insights WHERE stats_type = 'VideoInsights' AND title = 'Lifetime Total Video View Time (in MS)' AND sys_time > (CURRENT_DATE-2) AND proj_id_plat LIKE '" + projID.toString + "' GROUP BY sys_time ORDER BY sys_time DESC LIMIT 1;"
         Json.prettyPrint(Json.arr(Json.obj("count" -> numberFromQuery.first.statNumber)))
     }
     def queryTotalVideoViewsDateRange(implicit session: Session, projID: Long, start: String, stop: String) : String = {
@@ -151,17 +99,14 @@ trait BuildRedshiftQuery { this: PlainSqlRedshift =>
         Json.prettyPrint(Json.parse(sb.toString()))
     }
     def queryTop10Heatmap(implicit session: Session, projID: Long) : String = {
-        val queryResult = StaticQuery[IdValueTime] + "SELECT asset_id_plat, value, sys_time FROM fb_insights WHERE stats_type = 'DailyDiff' " +
-            "AND title LIKE '%Unique 10-Second Views%' AND sys_time < CURRENT_DATE and sys_time > CURRENT_DATE-8 AND asset_id_plat IN " +
-            "(SELECT asset_id_plat FROM fb_insights WHERE stats_type = 'DailyDiff' AND proj_id_plat like '" + projID.toString +
-            "' AND title LIKE '%Unique 10-Second Views%' AND sys_time < CURRENT_DATE AND sys_time > CURRENT_DATE-8 " +
-            "GROUP BY asset_id_plat ORDER BY SUM(value) DESC LIMIT 10)  ORDER BY asset_id_plat;"
+        val queryResult = StaticQuery[IdValueTime] + "SELECT * FROM top10_heatmap_" + projID.toString
         Json.toJson(queryResult.list).toString()
     }
     def queryTop10VideoIds(implicit session: Session, projID: Long) : List[KeyLongValuePair] = {
         val queryResult = StaticQuery[KeyLongValuePair] +
-            "SELECT asset_id_plat, SUM(value) FROM top10_heatmap WHERE proj_id_plat LIKE '" + projID.toString + "' GROUP BY asset_id_plat ORDER BY SUM(value) DESC;"
+            "SELECT asset_id_plat, SUM(value) FROM top10_heatmap_" + projID.toString + " GROUP BY asset_id_plat ORDER BY SUM(value) DESC;"
         queryResult.list
+
     }
 
 
@@ -294,7 +239,7 @@ trait BuildRedshiftQuery { this: PlainSqlRedshift =>
         val idList:Array[String] = vids.split(",").map(x => "'" + x + "'")
         val InCondition:String = idList.mkString(",")
         val queryResult = StaticQuery[KeyStrValuePair] +
-            "SELECT asset_id_plat, value FROM fb_insights WHERE stats_type = 'VideoInsights' AND title LIKE 'Lifetime Percentage of viewers at each interval%' AND sys_time LIKE (trunc(CONVERT_TIMEZONE ('UTC', 'PST', CURRENT_DATE)) || '%') AND asset_id_plat IN (" + InCondition + ");"
+            "SELECT asset_id_plat, value FROM fb_insights WHERE stats_type = 'VideoInsights' AND title LIKE 'Lifetime Percentage of viewers at each interval%' AND sys_time > (CURRENT_DATE-1) AND asset_id_plat IN (" + InCondition + ");"
         queryResult.list
     }
     def queryVideoRetentionBatch(implicit session: Session, vids: String) : String = {
